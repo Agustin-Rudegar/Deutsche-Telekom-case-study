5156e3af88526c22a7f48da549f17a55338551e5
Agustín Rüdegar <agustin.rudegar@gmail.com>
2026-04-18 12:09:14 +0200

    chore: setup vite project structure for vercel

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CaseStudyDeck from '../case_study_deck.jsx'
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0 }}>
+      <CaseStudyDeck />
+    </div>
+  )
+}
+
+export default App
